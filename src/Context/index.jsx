5656734c147f7aaad4341e74917b637ae460d9ba
mago_d_Mercurio <davila.feria@gmail.com>
2023-06-13 17:15:06 -0500

    add Show and hide ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,10 +4,35 @@ import { createContext, useState  } from "react"
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
+
+    //shopping cart increment quantity
     const [count, setCount] = useState(0)
     
+    //product detail open/close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+    
+        //product detail show product
+    const [productToShow, setProductToShow] = useState({
+        title: '',
+        price: '',
+        description: '',
+        images: [],
+    })
+
+
     return (
-        <ShoppingCartContext.Provider value={{count, setCount}}>
+        <ShoppingCartContext.Provider
+            value={{
+                count,
+                setCount,
+                isProductDetailOpen,
+                openProductDetail,
+                closeProductDetail,
+                productToShow,
+                setProductToShow,
+            }}>
             {children}
         </ShoppingCartContext.Provider>
     )
