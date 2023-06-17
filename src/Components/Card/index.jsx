f271147f627eb38ed5b5752d7d99d12829e1ad7a
mago_d_Mercurio <davila.feria@gmail.com>
2023-06-16 20:05:35 -0500

    Update index.jsx

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon, MinusIcon  } from '@heroicons/react/24/solid'
+
 
 const Card = ({ data }) => {
 
@@ -18,8 +19,28 @@ const Card = ({ data }) => {
     context.setCartProducts( [...context.cartProducts, productData] )
     context.openCheckoutSideMenu()
     context.closeProductDetail()
+  }
+
+  const renderIcon = (id) => {
+    const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+    if (isInCart) {
+      return (
+        <div className='absolute top-0 right-0 flex justify-center items-center bg-green-500  w-6 h-6 rounded-full m-2 p-1' >
+          <CheckIcon className='w-6 h-6 text-white' />
+          </div>
+      )
+    } else {
+      return (
+        <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' 
+        onClick={ ( event ) => addProductsToCart(event, data)} >
+          <PlusIcon className='w-6 h-6 text-black' />
+          </div>
+      )
+
+    }
 
 
+    
   }
 
   return (
@@ -29,10 +50,9 @@ const Card = ({ data }) => {
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.category.name}</span>
         <img className='w-full h-full object-cover rounded-lg rounde-lg ' src={data.images[0]} alt="headphones" />
-        <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' 
-        onClick={ ( event ) => addProductsToCart(event, data)} >
-          <PlusIcon className='w-6 h-6 text-black' />
-          </div>
+        {
+          renderIcon(data.id)
+        }
       </figure>
       <p className='flex justify-between'>
         <span className='text-xs font-light'>{data.title}</span>
